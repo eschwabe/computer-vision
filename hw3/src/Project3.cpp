71dd10c8997000517b08df74ef0b65ed133b86d1
Eric Schwabe <eschwabe@gmail.com>
2011-05-08 22:14:52 -0700

    Fix Mac OS X build

--- hw3/src/Project3.cpp
+++ hw3/src/Project3.cpp
@@ -2,6 +2,7 @@
 #include "math.h"
 #include "ui_mainwindow.h"
 #include <QtGui>
+#include <cmath>
 
 /*******************************************************************************
     HELPER METHODS
